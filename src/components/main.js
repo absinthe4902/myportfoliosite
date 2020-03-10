c4817fa1e10529aa526364d182943a4c2b04e05a
이승민 <seungmin.lee@dnx.kr>
2020-03-10 18:06:48 +0900

    ADD: first lesson of making portfolio

---
+++ src/components/main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import LandingPage from './landingpage';
+import AboutMe from './aboutme';
+import Contact from './contact';
+import Projects from './projects';
+import Resume from "./resume";
+
+const Main = () => (
+    <Switch>
+        <Route exact path="/" component={LandingPage}/>
+        <Route path="/about" component={AboutMe}/>
+        <Route path="/contact" component={Contact}/>
+        <Route path="/resume" component={Resume}/>
+        <Route path="/projects" component={Projects}/>
+    </Switch>
+);
+
+export default Main;
