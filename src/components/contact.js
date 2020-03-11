6f6cfdea846f883f203c1ada05bf166ea5925523
Seungmin Lee <seungmin.lee@dnx.kr>
2020-03-11 22:44:23 +0900

    ADD: protfolio lesson4

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,56 @@
 import React, {Component} from 'react';
+import {Grid, Cell, List, ListItem, ListItemContent} from "react-mdl";
+import mylogo from '../img/mylogo.png';
 
 class Contact extends Component {
     render() {
-        return(
-            <div><h1>Contact Page</h1></div>
+        return (
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>Jamie Lee</h2>
+                        <img src={mylogo} alt="avatar" style={{height: '250px'}}/>
+                        <p style={{width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                            galley of type and scrambled it to make a type specimen book. It has survived not only five
+                            centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                        </p>
+                    </Cell>
+                    <Cell col={6} className="contact-cell">
+                        <h2>Contact Me</h2>
+                        <hr/>
+                        <div className="contact-list">
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                                        <i className="fa fa-phone-square" aria-hidden="true"/>
+                                        (123) 456-7890
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                                        <i className="fa fa-fax" aria-hidden="true"/>
+                                        (123) 456-7890
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                                        <i className="fa fa-envelope" aria-hidden="true"/>
+                                        someone@gamil.com
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                                        <i className="fa fa-skype" aria-hidden="true"/>
+                                        MySkypeId
+                                    </ListItemContent>
+                                </ListItem>
+
+                            </List>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
         );
     }
 }
